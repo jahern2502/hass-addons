1c0441244bcba6b0bdb5dd59e7a3805532aa3a8c
John Ahern <john.ahern@infotrack.com.au>
2019-04-16 19:06:02 +1000

    refactor

--- xboxone/run.sh
+++ xboxone/run.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 # Copy over the xbox component
-mkdir -p /config/custom_components/media_player
-cp -f xboxone.py /config/custom_components/media_player/xboxone.py
+mkdir -p /config/custom_components/xboxone
+cp -f xboxone.py /config/custom_components/xboxone/media_player.py
 
+# Persistent tokens on reboot
 touch /config/.xbox-token.json
-
 mkdir -p /root/.local/share/xbox/
-rm /root/.local/share/xbox/tokens.json
 ln -s /config/.xbox-token.json /root/.local/share/xbox/tokens.json
+
 
 # Run the server
 xbox-rest-server
+
